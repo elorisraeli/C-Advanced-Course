3d6deaf0042e57a3766e92ccc5adc0aea37702b5
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2022-09-17 19:29:23 +0300

    pack data with bits

--- Section8/bitmasks.c
+++ Section8/bitmasks.c
@@ -3,6 +3,8 @@
 
 // masks can use to turn on and off bits and to verify bits in numbers
 
+#define BYTE_MASK 0xff
+
 int main(){
     int results[4];
 
@@ -36,7 +38,40 @@ int main(){
     }
 
 
+    /*
+    if we want to store data in bits like a pack,
+    we can calculate how many bits we need to use:
+    example:
+    we need to store 3 flags (1 bit each), a number between 1-255 (8 bits),
+    and a number between 1-100,000 (18 bits).
+    To sum up, we need 29 bits and this is the reason we need storage with 32 bits in this case.
 
+    unused   f1  f2  f3      type (1-255)           index (1-100,000)
+    | 0 0 0 | 0 | 0 | 0 | 0 0 0 0 0 0 0 0 | 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 |
+        3     1   1   1          8                          18                   
+
+   unsigned int packed_data; // 32 bits on the most systems
+   packed_data = (packed_data | 7) << 18; // change the value of the type bits, OR with 18 bits left
+   packed_data &= 0xfc03ffff; // packed_data & (1111 1100 0000 0011 1111 1111 1111 1111)
+
+   to extract the type field of packed_data and assign it to n:
+   n = (packed_data >> 18) & 0xff; // its drop the 18 bits that right and leaves the next bits
+   // with the operator AND and the number 1111 1111 then you get what you want
+
+    */
+
+   unsigned int data = 99; // 0110 0011
+   unsigned int result1 = (data >> 4);
+   unsigned int result2 = (data >> 4) & 0xf;
+    printf("0xf = %u, %d\n", 0xf, 0xf);
+    printf("data = %u, result1 = %u, result2 = %u\n", data, result1, result2);
+    printf("AWESOME!!\n");
+
+    unsigned long color = 0x002a162f; // 0000 0000 0010 1010 0001 0110 0010 1111
+    unsigned char blue, green, red;
+    red = color & BYTE_MASK; // 0010 1111
+    green = (color >> 8) & BYTE_MASK; // 0001 0110
+    blue = (color >> 16) & BYTE_MASK; // 0010 1010
 
 
     return 0;
