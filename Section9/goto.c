cadee2ebb5057c7c9c9aff8dd029ca58cff5c0d9
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2022-09-18 10:40:44 +0300

    challenge added

--- Section9/goto.c
+++ Section9/goto.c
@@ -6,8 +6,10 @@ its mess the control flow of the program and its harder to follow.
 instead we can use: 'continue;' and 'break;'.
 */
 
+int main()
+{
+    /*
 
-int main(){
     // example of using goto statement:
     // example 1:
     const int calls = 5;
@@ -22,11 +24,11 @@ int main(){
         }
         total += num;
     }
-    // if the num is less than 1 gets here
-    // if the loop ends get here anyway
-    anotherLine:
-        average = total/i;
-        printf("The average is: %d\n\n", average);    
+// if the num is less than 1 gets here
+// if the loop ends get here anyway
+anotherLine:
+    average = total / i;
+    printf("The average is: %d\n\n", average);
 
     // example 2:
     const int max = 10;
@@ -37,7 +39,7 @@ int main(){
         {
             for (int k = 0; k < 10; k++) // run 5x6x10 times
             {
-                sum = i+j+k;
+                sum = i + j + k;
                 printf("The sum of vars is: %d\n", sum);
                 // with the max we can control how many times the loop execute
                 if (sum == max)
@@ -48,9 +50,51 @@ int main(){
         }
     }
 
-    someLine:
-        printf("WE GET TO MAX SUM. (%d)", sum);
+someLine:
+    printf("WE GET TO MAX SUM. (%d)", sum);
 
-    
+    */
+
+    // Challenge:       ("Spaghetti loop", not good programming..)
+    int value = 18;
+    int i = 1, j, k= 0;
+
+    outerLoop:
+        if (i < value) {
+           j = i;
+
+           innerLoop:
+              if (j < value) {
+                printf(" ");
+                j++;
+                goto innerLoop;
+              }
+              else {
+                 innerLoop2:
+                    if (k != ((2 * i)))  {
+                        if (k == 0 || k == (2 * i) - 3) {
+                             printf("*");
+                        }
+                        printf(" ");
+                        k++;
+                        goto innerLoop2;
+                    }
+
+                    k = 0;
+                    printf("\n");
+                    i++;
+                    goto outerLoop;
+              }
+        }
+        else {
+           i = 0;
+           loop:
+             if (i < (2 * value) - 1) {
+               printf("*");
+               i++;
+               goto loop;
+             }
+        }
+
     return 0;
 }
