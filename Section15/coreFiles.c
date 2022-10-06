40a8503996232cf9d67b97c574c4f2dc1ef28830
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2022-10-06 21:01:47 +0300

    core files examples

---
+++ Section15/coreFiles.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+// generated if the program crashed (by division by zero or illegal memory access)
+// this file create for debugging purposes
+// we will use gdb to analyze our core files
+
+void main(){
+    // char *str = "GfG";
+    // // problem: trying to modify read only memory
+    // *(str+1) = 'n';
+
+    // another problem
+    // int *p = malloc(8);
+    // *p = 100;
+    // free(p);
+    // *p = 110;
+
+    // another problem
+    // int array[2] = {1, 2};
+    // array[3] = 10;
+
+    // another problem
+    // int n = 2;
+    // scanf("%d", n);
+    
+}
